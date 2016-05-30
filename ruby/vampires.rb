9efa5978b1ba2395e46094b90c105c29022f0e13
Long Tran <artsandcrass@gmail.com>
2016-05-30 02:38:25 -0700

    finished assignment. Added vampire_checker method

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,76 +1,97 @@
-puts "What is your name?"
-	hire_name = gets.chomp
-wrong1_name = true
-wrong2_name = false
-unknown_name = "Results are inconclusive"
-	if hire_name == "Drake Cula"
-		hire_name = wrong_name1
-	elsif hire_name == "Tu Fang"
-		hire_name = wrong_name2
-	else
-	    hire_name = unknown_name
+
+def vampire_checker
+
+	def age_plus_year(x,y)
+		  x.to_i + y.to_i
+	end
+	#usersinput
+	puts "What is your name?"
+		hire_name = gets.chomp
+
+	puts "How old are you?"
+	  hire_age = gets.chomp
+
+	puts "What year are you born?"
+	  hire_birthyr = gets.chomp
+
+	puts "Our company cafeteria serves garlic bread, should we order some for you, yes or no?"
+		hire_garlic = gets.chomp
+
+	puts "Would you like to enroll in the company's health insurance? Yes or no?"
+		hire_insurance = gets.chomp
+
+  #loop allergies
+	is_allergy_sunshine = false
+	hire_allergy = nil
+	until hire_allergy == "done" || is_allergy_sunshine
+		puts "Name an allergy:"
+		hire_allergy = gets.chomp
+		if hire_allergy == "sunshine"
+			is_allergy_sunshine = true
+		end
+	end
+	#booleans
+
+	is_name_vamp = true
+	is_age_correct = false
+	is_garlic_ok = false
+	is_sign_insurance = false
+
+
+	# check name
+	if hire_name != "Drake Cula" && hire_name != "Tu Fang"
+		is_name_vamp = false
 	end
 
-puts "How old are you?"
-	hire_age = gets.chomp
+	# check age
+	current_year = 2016
+	sum_age_plus_year = age_plus_year(hire_age,hire_birthyr)
 
-puts "What year are you born?"
-	hire_birthyr = gets.chomp
+	if current_year == sum_age_plus_year
+			is_age_correct = true
+	end
 
-def age_plus_year(x,y)
-	age_plus_year = x + y
+	# check garlic
+
+	if hire_garlic == "yes"
+		is_garlic_ok = true
+	end
+
+	# check insurance
+
+	if hire_insurance == "yes"
+		is_sign_insurance = true
+	end
+
+	# Check Booleans
+
+	if is_name_vamp == true
+		puts "Definitely a Vampire"
+	elsif is_allergy_sunshine == true
+		puts "Probably a Vampire"
+	elsif is_age_correct && (is_garlic_ok || is_sign_insurance)
+		puts "Probably not a Vampire"
+	elsif !is_age_correct && (!is_garlic_ok && !is_sign_insurance)
+		puts "Almost certainly a vampire!"
+	elsif !is_age_correct && (!is_garlic_ok || !is_sign_insurance)
+		puts "Probably a Vampire"
+	else
+		puts "Results inconclusive"
+	end
+
 end
 
-age_plus_year(hire_age,hire_birthyr) = sum_of_year
-current_year = true
-age_wrong = false
-if age_plus_year == 2016
-	age_plus_year = current_year
-else
-	age_plus_year = age_wrong
+#users input
+puts "How many employees will be processed?"
+amount_to_process = gets.chomp
 
+#loop
+
+employee_process = 0
+
+while employee_process < amount_to_process.to_i
+	 vampire_checker
+	employee_process += 1
 end
 
-puts "Our company cafeteria serves garlic bread, should we order some for you, yes or no?"
-	hire_garlic_preference = gets.chomp
-
-will_eat = true
-wont_eat = false
-if hire_garlic_preference == "yes"
-	hire_garlic_preference = will_eat
-else
-	hire_garlic_preference = wont_eat
-end
-
-
-puts "Would you like to enroll in the company's health insurance? Yes or no?"
-	hire_insurance = gets.chomp
-
-will_signup = true
-wont_signup = false
-if hire_insurance == "yes"
-	hire_insurance = will_signup
-else
-	hire_insurance = wont_signup
-end
-current_year = true
-unknown_name = "Results are inconclusive"
-will_signup = true
-wont_signup = false
-will_eat = true
-wont_eat = false
-
-age_wrong = false
-wrong1_name = true
-wrong2_name = false
-if age_right && will_eat && will_signup
-	puts "Probably not a vampire"
-elsif age_wrong || wont_eat || wont_signup
-	puts "Probably a vampire"
-elsif age_wrong || wont_eat || wont_signup
-	puts "Almost certainly a vampire"
-elsif wrong_name1 || wrong_name2
-	puts "Definitely a Vampire"
-else
-	puts unknown_name
-end
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
