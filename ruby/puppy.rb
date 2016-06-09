580369d432d3e82964f8c37a1f8167c4c62666ce
Long Tran <artsandcrass@gmail.com>
2016-06-09 10:46:31 -0700

    initial commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Algorithm for puppies
+
+# identify the type of dog breed
+# a description of the dog
+  # name of dog
+  # color of coat
+  # eye color
+  # age
+  # immunization status
+
+# how it behaves and its mannerisms
+  # personality type
+  # is he/she active?
+  # get along with others?
