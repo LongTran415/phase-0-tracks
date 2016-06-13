11c61222eb1b319ae5a50d7b497d2b8affaf51fe
Long Tran <artsandcrass@gmail.com>
2016-06-12 21:40:54 -0700

    finsihed adding getter and setter functions

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,20 +5,24 @@
   # reindeer_ranking
 
 class Santa
+attr_reader :name, :ice_cream
+attr_accessor :name, :ice_cream
 
-  def initialize (gender, ethnicity, hair_color)
+  def initialize (gender, ethnicity, hair_color, age, ice_cream, name)
      puts "Initializing Santa instance ..."
+     @name = name
      @gender = gender
      @ethnicity = ethnicity
      @hair_color = hair_color
-     puts "Gender: #{@gender}"
-     puts "Ethnicity: #{@ethnicity}"
-     puts "Hair color: #{@hair_color}"
+     @age = age
+     @ice_cream = ice_cream
+
   end
 
   def reindeer_fav_sorter(arry)
-    arry = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    arry.sort_by = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
+
 
   def age(integer)
     integer += 0
@@ -26,7 +30,7 @@ class Santa
   end
 
   def speak
-    puts "Ho, ho, ho! Happy holidays!"
+    puts "#{@name} Says Ho, ho, ho! Happy holidays!"
   end
 
   def eat_milk_and_cookies(cookie_type)
@@ -34,25 +38,71 @@ class Santa
   end
 
   def about
+    puts "Name: #{@name}"
     puts "Gender: #{@gender}"
     puts "Ethnicity: #{@ethnicity}"
+    puts "Ice Cream pref: #{@ice_cream}"
+    puts "Age: #{@age}"
   end
 
 
+#>>> getter methods make code readable
+#   def name
+#     @name
+#   end
+
+#   def ice_cream
+#     @ice_cream
+#   end
+
+#>>> setter method makes code writable
+#   def name=(new_name)
+#     @name = new_name
+#   end
+
+#   def ice_cream=(ice_cream_flavor)
+#     @ice_cream = ice_cream_flavor
+#   end
+
 end
 
-santa = Santa.new("male", "unicorn", "blue")
-santa.age(4)
+
+# driver code for initializing santas
+
+santa = Santa.new("male", "Unicorn", "blue", 22, "Orange cream", "Zoro")
+santa.name = "Zoro"
+santa.age(22)
+santa.about
 santa.speak
 santa.eat_milk_and_cookies("chocolate chip")
 
+
+santa = Santa.new("male", "Merman", "Green", 21, "Banana nut", "Luffy")
+santa.name = "Luffy"
+santa.age(21)
+santa.ice_cream = "Chocolate"
+santa.about
+santa.speak
+santa.eat_milk_and_cookies("chocolate, chocolate chip")
+
+# utilizing arrays
+
 santas = []
 
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_eye_color = ["Blue", "Green", "Brown", "Rainbow", "Violet", "Red", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i], example_eye_color[i])
-end
 
-# driver code for initializing santas
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+example_hair_color = ["Blue", "Green", "Brown", "Rainbow", "Violet", "Red", "N/A"]
+
+example_ice_cream = ["Vanilla", "Caramel", "Strawberry", "Neapolitan", "", "Chocolate", "N/A"]
+
+example_names = ["Charlie", "Kim", "Rupert", "Mehul", "", "Lisa", "Bart"]
+
+example_age = [14, 45, 55, 21, 28, 33, 4]
+
+
+example_genders.length.times do |i|
+  santas << santa = Santa.new(example_genders[i], example_ethnicities[i], example_hair_color[i], example_age[i], example_ice_cream[i], example_names[i]).about
+end
