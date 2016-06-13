2b738aeaecaddcc082ca0053722691865552f59f
Long Tran <artsandcrass@gmail.com>
2016-06-13 01:07:43 -0700

    initial commit. added algorithm

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+
+# Design a class for wizards
+
+# Add attributes
+  #name
+  #age
+  #race
+  #power_type
+  #location
+
+# Create wizard methods with an argument
+  # steps:
+    # define method that make sense to wizards
+    # give some methods arugments
