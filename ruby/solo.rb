8978b9c798275079fe16340ba844a1aede5524a5
Long Tran <artsandcrass@gmail.com>
2016-06-13 20:13:24 -0700

    finished assignment and algorithm

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -14,10 +14,11 @@
     # give some methods arugments
 
 class Wizard
+
   #>>>>>>>>>>>>>
   # getter/setter methods
-  attr_reader :name, :age, :race, :power_type, :location
-  attr_accessor :name, :age, :race, :power_type, :location
+  attr_reader :name, :age, :race, :power_type, :location, :profile
+  attr_accessor :name, :age, :race, :power_type, :location, :profile
 
   def initialize(name, age, race, power_type, location)
     puts "Creating wizard class..."
@@ -29,7 +30,7 @@ class Wizard
   end
 
   def age(integer)
-    integer += 0
+    integer.to_i += 0
     puts "Age: #{integer}"
   end
 
@@ -45,18 +46,49 @@ class Wizard
     @power_type = school_of_magic
   end
 
-
   def profile
     puts "Name: #{@name}"
     puts "Age: #{@age}"
     puts "Race: #{@race}"
     puts "Power_type: #{@power_type}"
     puts "Location: #{@location}"
+
   end
 
 end
 
-wizard = Wizard.new("Li Ming", "300", "Human", "Arcane", "Wizard School")
+#driver code
+
+wizard = Wizard.new("Li Ming", 300,"Human", "Arcane", "Wizard School")
 wizard.profile
 wizard.cast_spell("Fire Ball!!")
 wizard.teleport("Harry's place")
+
+#user interface
+
+user_input = nil
+
+
+until user_input == "done"
+
+	puts "Wizard profile maker. Name, age, race, power_type, and location in that order please:"
+
+	user_input = gets.chomp
+
+
+	user_descript = user_input.split(" ")
+	#=> ["li", "23", "human", "ice", "russia"]
+
+	wizard_name = user_descript[0]
+	wizard_age = user_descript[1]
+	wizard_race = user_descript[2]
+	wizard_power = user_descript[3]
+	wizard_loca = user_descript[4]
+
+	wizard = Wizard.new(wizard_name, wizard_age, wizard_race,wizard_power,wizard_loca).profile
+
+ 	puts "Are you done, if yes type done, otherwise hit enter."
+
+ 	user_input = gets.chomp
+
+ end
