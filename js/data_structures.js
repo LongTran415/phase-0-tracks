fd519dc107da838b1a8e5f3e5296c88aa9b71cb9
Long Tran <artsandcrass@gmail.com>
2016-06-16 14:37:31 -0700

    added driver code to test, created new objects and looped the keys in the object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,6 @@
 
 
+// Release 1
 
 var horse_color = ["Yellow", "Teal", "Violet", "Red"]
 
@@ -9,12 +10,21 @@ horse_color.push("Purple")
 
 horse_name.push("Bob")
 
-// console.log(horse_color);
+console.log(horse_color);
+
+
+// Release 2
+
 
 // literal construction
 var horse = {"Ed": "Yellow", "Rudy": "Teal", "Steven": "Violet", "Rambo": "Red", "Bob": "Purple"};
 
+
 console.log(horse);
+
+
+
+// Release 3
 
 function Horse(name, color){ console.log("Our new horse:", this);
       this.name = name;
@@ -27,4 +37,21 @@ function Car(type, color){
       this.color = color;
       this.take_off = function(){ console.log("Zoooooom!");
 };
-                 
+}
+
+// driver code
+var fastCar = new Car("Corvett", "Red");
+
+fastCar.take_off();
+console.log(fastCar)
+
+// driver code
+var workCar = new Car("Pick up truck", "White");
+console.log(car_arry);
+
+onsole.log(workCar)
+
+// driver code loop
+for (var obj in horse) {
+  console.log(obj)
+}
